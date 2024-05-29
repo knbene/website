c0ecc3e8a3c154c0194013a53c5495e41b31c8e7
Bene <KnoepfleB@outlook.de>
2024-05-30 00:25:57 +0200

    Add files via upload

---
+++ fixed_v.0.0.2-29.05.2024/script.js
@@ -0,0 +1,14 @@
+function bene() {
+    window.location.href = '/';
+}
+function links() {
+    window.location.href = '/links/';
+}
+
+function projects() {
+    window.location.href = '/projects/';
+}
+
+function login() {
+    window.location.href = 'https://dashboard.nxBene.de';
+}
